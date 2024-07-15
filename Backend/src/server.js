1b59632d9059e79a70498846d1b1b66cec0c8647
Aayush Anand <btech10098.22@bitmesra.ac.in>
2024-07-15 21:44:34 +0530

    Cloud setup

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const connectToMongoDB = require("./db/connectToMongoDB.js");
+var cors = require('cors');
 const inventory = require('./routes/inventory.js')
-app.use('/inventory',inventory);
 
-app.get('/',(req,res)=>{
-    res.send('home page'); 
+connectToMongoDB();
+app.use(cors());
+
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use('/inventory', inventory);
+
+app.get('/', (req, res)=>{
+    res.send('Home Page'); 
 })
 
-app.listen(PORT,()=>{
-    console.log(`app listening on port ${PORT}`)
+app.listen(PORT, ()=>{
+    console.log(`App is listening on port ${PORT}`)
 });
